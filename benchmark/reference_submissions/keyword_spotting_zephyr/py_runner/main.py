10c1614135c790c9cd505d87ea1f5fe78d9c27bc
Anders Granskogen Bjørnstad <anders.bjornstad@axbit.com>
2025-03-12 22:24:49 +0100

    kws-zephyr: Switch main.py to using serial_device.py
    Now works with read-listener too.

--- benchmark/reference_submissions/keyword_spotting_zephyr/py_runner/main.py
+++ benchmark/reference_submissions/keyword_spotting_zephyr/py_runner/main.py
@@ -4,6 +4,7 @@ import polars as pl
 import serial
 import time
 
+from serial_device import SerialDevice
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -15,45 +16,41 @@ if __name__ == "__main__":
         .rename({"column_1": "file", "column_2": "n_classes", "column_3": "class"})
     )
 
-    def write(ser, command):
-        ser.write(command.encode('utf-8'))
-        print(f"TX> {command}")
+    def send_command(ser, command):
+        ser.send_command(command)
         time.sleep(0.05)
 
     port = '/dev/ttyACM1'  # Replace with your serial port
     baudrate = 115200
     chunk_size = 72
     try:
-        # Open the serial port
-        ser = serial.Serial(port, baudrate)
-        print(f"Connected to {port} at {baudrate} baud")
+        # Open the serial port with echo enabled
+        with SerialDevice(port, baudrate, delimiter="%") as ser:
+            ser._echo = True
+            print(f"Connected to {port} at {baudrate} baud")
+            send_command(ser, "help")
+            for row in y_labels.head(1).iter_rows(named=True):
+                with open(args.dataset_path / row["file"], "rb") as file:
+                    binary_data = file.read()
+                n_bytes = len(binary_data)
 
-        for row in y_labels.head(5).iter_rows(named=True):
-            with open(args.dataset_path / row["file"], "rb") as file:
-                binary_data = file.read()
-            n_bytes = len(binary_data)
+                # Send the "db load" command
+                command = f"db load {n_bytes}"
+                send_command(ser, command)
 
-            # Send the "db load" command
-            command = f"db load {n_bytes}%"
-            write(ser, command)
+                # Convert binary data to hex representation
+                hex_data = ''.join([f'{byte:02x}' for byte in binary_data])
 
-            # Convert binary data to hex representation
-            hex_data = ''.join([f'{byte:02x}' for byte in binary_data])
-
-            # Send the hex data in chunks
-            for i in range(0, len(hex_data), chunk_size):
-                chunk = hex_data[i:i + chunk_size]
-                chunk_command = f"db {chunk}%"
-                write(ser, chunk_command)
-
-            write(ser, "infer 1 0%")
-            print(f"Sent: {args.dataset_path / row['file']}, ground truth {row["class"]}")
-
+                # Send the hex data in chunks
+                for i in range(0, len(hex_data), chunk_size):
+                    chunk = hex_data[i:i + chunk_size]
+                    chunk_command = f"db {chunk}"
+                    send_command(ser, chunk_command)
+                send_command(ser, "infer 1 0")
+                print(f"Sent: {args.dataset_path / row['file']}, ground truth {row["class"]}")
+                time.sleep(0.2)
+                print("--------------------------------")
     except serial.SerialException as e:
         print(f"Error: Could not open serial port: {e}")
     except KeyboardInterrupt:
         print("Exiting...")
-    finally:
-        if 'ser' in locals() and ser.is_open:
-            ser.close()
-            print("Serial port closed.")
