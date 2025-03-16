5f4d4d451da052d404d756048305eb647ce08128
Anders Granskogen Bjørnstad <anders.bjornstad@axbit.com>
2025-03-16 18:23:06 +0100

    kws-zephyr: py_runner: Parse and compare inference results
    Run through entire dataset, printing some useful info and collect
    wrong predictions (if any).

--- benchmark/reference_submissions/keyword_spotting_zephyr/py_runner/main.py
+++ benchmark/reference_submissions/keyword_spotting_zephyr/py_runner/main.py
@@ -1,10 +1,36 @@
 import argparse
+import numpy as np
 from pathlib import Path
 import polars as pl
 import serial
 import time
 
 from serial_device import SerialDevice
+
+def parse_infer_results(res: list[str]):
+    """
+    res may look something like:
+    [ 'm-ready', 'm-warmup-start-0', 'm-warmup-done', 'm-infer-start-3',
+      'm-lap-us-2077028000', 'm-lap-us-2077237000', 'm-infer-done',
+      'm-results-[0.000,0.000,0.000,0.000,0.000,0.089,0.000,0.000,0.000,0.000,0.000,0.910]'
+    ]
+    """
+    n_inference = 0
+    for i, line in enumerate(res):
+        if "m-infer-start" in line:
+            n_inference = int(line[len("m-infer-start-"):])
+            break
+    assert n_inference > 0
+    assert "m-lap-us-" in res[i + 1]
+    start_us = int(res[i + 1][len("m-lap-us-"):])
+    assert "m-lap-us-" in res[i + 2]
+    end_us = int(res[i + 2][len("m-lap-us-"):])
+    assert "m-results-" in res[i + 4]
+    results = res[i + 4][len("m-results-"):]
+    results = results.strip('[]')  # Remove square brackets
+    results = [float(x) for x in results.split(",")]
+    print(f"infer {n_inference} took {(end_us - start_us)/1e3}ms, class {np.argmax(results)} ({np.max(results)})")
+    return np.argmax(results)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -17,19 +43,22 @@ if __name__ == "__main__":
     )
 
     def send_command(ser, command):
-        ser.send_command(command)
+        res = ser.send_command(command)
+        print(f"RX> {res}")
         time.sleep(0.05)
+        return res
 
     port = '/dev/ttyACM1'  # Replace with your serial port
     baudrate = 115200
     chunk_size = 72
     try:
         # Open the serial port with echo enabled
-        with SerialDevice(port, baudrate, delimiter="%") as ser:
-            ser._echo = True
+        with SerialDevice(port, baudrate, delimiter="%", end_of_response="<EOR>") as ser:
+            ser._echo = False
             print(f"Connected to {port} at {baudrate} baud")
             send_command(ser, "help")
-            for row in y_labels.head(1).iter_rows(named=True):
+            wrong_predictions = set()
+            for n_row, row in enumerate(y_labels.iter_rows(named=True)):
                 with open(args.dataset_path / row["file"], "rb") as file:
                     binary_data = file.read()
                 n_bytes = len(binary_data)
@@ -45,11 +74,20 @@ if __name__ == "__main__":
                 for i in range(0, len(hex_data), chunk_size):
                     chunk = hex_data[i:i + chunk_size]
                     chunk_command = f"db {chunk}"
-                    send_command(ser, chunk_command)
-                send_command(ser, "infer 1 0")
-                print(f"Sent: {args.dataset_path / row['file']}, ground truth {row["class"]}")
-                time.sleep(0.2)
+                    res = send_command(ser, chunk_command)
+                assert "m-load-done" in res
+                res = send_command(ser, "infer 1 0")
+                assert "m-infer-done" in res
+                pred = parse_infer_results(res)
+                print(f"Sent: {args.dataset_path / row['file']}, ground truth {row["class"]}, pred {pred}")
+                if pred != row["class"]:
+                    wrong_predictions.add(n_row)
+                    print(f"Wrong prediction: {args.dataset_path / row['file']}, ground truth {row["class"]}, pred {pred}")
+                else:
+                    print("Correct prediction")
+                time.sleep(0.1)
                 print("--------------------------------")
+            print(f"Wrong predictions: {wrong_predictions}")
     except serial.SerialException as e:
         print(f"Error: Could not open serial port: {e}")
     except KeyboardInterrupt:
